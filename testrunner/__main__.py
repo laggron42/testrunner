ad43d2b6d3aedc6d5fb59410841e433d700c6e04
laggron42 <laggron42@gmail.com>
2022-09-22 10:52:26 +0200

    main: basic configuration for test run

--- testrunner/__main__.py
+++ testrunner/__main__.py
@@ -1,7 +1,11 @@
 import argparse
 import sys
 
+from pathlib import Path
+
 from testrunner import __version__ as program_version
+from testrunner.yaml import Config
+from testrunner.runner import GroupRunner
 
 
 class CLIFlags(argparse.Namespace):
@@ -20,3 +24,11 @@ def main():
     if cli_flags.version:
         print(f"Testrunner version {program_version}")
         sys.exit(0)
+
+    config = Config(Path("tests.yml"))
+    runner = GroupRunner(config.groups[0])
+    print(list(runner._run_tests()))
+
+
+if __name__ == "__main__":
+    main()
